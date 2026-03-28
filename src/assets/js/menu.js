9b3187d6f6dfc3690a7db51ebb029709642f290c
kelly-Moon <kellymoon220@gmail.com>
2026-03-28 13:53:32 +0900

    헤더 완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,21 @@
 export function menu() {
-  console.log(menu);
+  // console.log(menu);
+  const headerToggle = document.querySelector("#headerToggle");
+  const heafeNav = document.querySelector(".header_nav");
+
+  if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+      heafeNav.classList.toggle("show");
+
+      headerToggle.getAttribute("aria-expanded") === "true"
+        ? headerToggle.setAttribute("aria-expanded", "false")
+        : headerToggle.setAttribute("aria-expanded", "true");
+    });
+  }
+
+  // if (headerToggle.getAttribute("aria-expanded") === "true") {
+  //   headerToggle.setAttribute("aria-expanded", "false");
+  // } else {
+  //   headerToggle.setAttribute("aria-expanded", "true");
+  // }
 }
