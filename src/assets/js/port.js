f712ce050371ae56118c5a5347ac0ef488cf25e3
kelly-Moon <kellymoon220@gmail.com>
2026-03-30 00:23:30 +0900

    수정

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -6,6 +6,7 @@ export function port() {
 
   const horizontal = document.querySelector("#port");
   const horSection = gsap.utils.toArray(".port_item");
+  // const wrap = horizontal.querySelector(".port_wrap");
 
   gsap.to(horSection, {
     xPercent: -120 * (horSection.length - 1),
@@ -13,10 +14,11 @@ export function port() {
     scrollTrigger: {
       trigger: "#port",
       start: "top 56px",
-      end: "+=3000",
+      // end: "+=3000",
+      eend: () => "+=" + horizontal.offsetWidth,
       pin: true,
       scrub: 1,
-      markers: true,
+      markers: false,
       invalidateOnRefresh: true, //refresh될 때마다 다시 계산
       anticipatePin: 1, //pin 시작할 때 화면 튀는 현상(jump)을 줄여주는 옵션
     },
